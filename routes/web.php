003cd6e90e41183ba1f7a8dc11b0912f664c2adc
ASBarros <anderson.barros0298@gmail.com>
2020-11-12 18:43:39 -0300

    criando o controller dos instrutores

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CadInstrutoresController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::post('/painel', [UsuarioController::class, 'login'])->name('usuario.login');
+Route::post('/instrutores', [CadInstrutoresController::class, 'index'])->name('instrutores.cadastro');
